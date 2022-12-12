f8b08f445f2bf2cfe965947a738e9eec49bc9a9d
amnamaharani <amnamaharani10@gmail.com>
2022-12-12 18:00:48 +0100

    initialize Firebase

--- flutter_twitter/lib/main.dart
+++ flutter_twitter/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_twitter/Screens/welcome_screen.dart';
+import 'package:flutter_twitter/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  )
+  ;
   runApp(const MyApp());
 }
 
