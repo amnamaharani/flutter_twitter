0786615a30ee638b77669a3f52424f2eadc59f84
amnamaharani <amnamaharani10@gmail.com>
2022-12-12 22:38:36 +0100

    Add Container : photoProfile + edit button

--- flutter_twitter/lib/Screens/profile_screen.dart
+++ flutter_twitter/lib/Screens/profile_screen.dart
@@ -75,7 +75,75 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         image: NetworkImage(userModel.coverImage),
                       )
                 ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox.shrink(),
+                      PopupMenuButton(
+                        icon: const Icon(
+                          Icons.more_horiz, 
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        itemBuilder: (context) {
+                          return <PopupMenuItem<String>>[
+                            const PopupMenuItem(
+                              value: 'logout',
+                              child: Text('Logout'),
+                            )
+                          ];
+                        },
+                        onSelected: (selectedItem) {
+                          
+                        },
+                      )
+                    ],
+                  ),
+                ),
               ),
+              Container(
+                transform: Matrix4.translationValues(0.0, -45.0, 0.0),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      radius: 45,
+                      backgroundImage: userModel.profilePicture.isEmpty
+                        ? null
+                        : NetworkImage(userModel.profilePicture),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 25,
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        border: Border.all(color: KTweeterColor)
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Edit',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: KTweeterColor,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           );
         }
