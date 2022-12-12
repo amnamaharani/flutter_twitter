c40983637186cb815a38cb194c698944a1337294
amnamaharani <amnamaharani10@gmail.com>
2022-12-12 19:17:47 +0100

    Add BottomNavBar

---
+++ flutter_twitter/lib/Screens/create_tweet_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateTweetScreen extends StatelessWidget {
+  const CreateTweetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('CreateTweetScreen'),
+      ),
+    );
+  }
+}
--- flutter_twitter/lib/Screens/feed_screen.dart
+++ flutter_twitter/lib/Screens/feed_screen.dart
@@ -1,13 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_twitter/screens/create_tweet_screen.dart';
+import 'package:flutter_twitter/screens/home_screen.dart';
+import 'package:flutter_twitter/screens/notification_screen.dart';
+import 'package:flutter_twitter/screens/profile_screen.dart';
+import 'package:flutter_twitter/screens/search_screen.dart';
 
-class FeedScreen extends StatelessWidget {
+class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
+
+  @override
+  State<FeedScreen> createState() => _FeedScreenState();
+}
+
+class _FeedScreenState extends State<FeedScreen> {
+  int selectedTab = 0;
+  List<Widget> feedScreens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const NotificationScreen(),
+    const ProfileScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HI'),
+      body: feedScreens.elementAt(selectedTab),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { 
+          Navigator.push(context, 
+          MaterialPageRoute(builder: ((context) => const CreateTweetScreen())
+          ));
+        },
+        backgroundColor: Colors.white,
+        child: Image.asset('assets/tweet.png'),
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        onTap: (index) {
+          setState(() {
+            selectedTab = index;
+          });
+        },
+        activeColor: const Color(0xff00acee),
+        currentIndex: selectedTab,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home)),
+          BottomNavigationBarItem(icon: Icon(Icons.search)),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications)),
+          BottomNavigationBarItem(icon: Icon(Icons.person)),
+        ],
       ),
     );
   }
