d6a4694fb8181dd3b8e279d2109422a9c24d0ea5
amnamaharani <amnamaharani10@gmail.com>
2022-12-12 22:08:33 +0100

    update cover image in profile

--- flutter_twitter/lib/Screens/profile_screen.dart
+++ flutter_twitter/lib/Screens/profile_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_twitter/model/user_model.dart';
+import 'package:flutter_twitter/service/database_service.dart';
+import 'package:flutter_twitter/theme/colors.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   final String currentUserId;
   final String visitedUserId;
 
@@ -11,10 +14,71 @@ class ProfileScreen extends StatelessWidget {
   }): super (key: key);
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int _followersCount = 0;
+  int _followingCount = 0;
+
+  getFollowersCount() async {
+    int followersCount = await DatabaseServices.followersNum(widget.visitedUserId);
+    if (mounted) {
+      setState(() {
+        _followersCount = followersCount;
+      });
+    }
+  }
+
+  getFollowingCount() async {
+    int followingCount = await DatabaseServices.followingNum(widget.visitedUserId);
+    if (mounted) {
+      setState(() {
+        _followersCount = followingCount;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getFollowersCount();
+    getFollowingCount();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('ProfileScreen'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: FutureBuilder(
+        future: usersRef.doc(widget.visitedUserId).get(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if(!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(KTweeterColor),
+              ),
+            );
+          }
+          UserModel userModel = UserModel.fromDoc(snapshot.data);
+          return ListView(
+            physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+            children: [
+              Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  color: KTweeterColor,
+                  image: userModel.coverImage.isEmpty
+                    ? null
+                    : DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(userModel.coverImage),
+                      )
+                ),
+              ),
+            ],
+          );
+        }
       ),
     );
   }
